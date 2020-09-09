9c82f7248c1b8bd4ed105b67f0e37e17619280ee
YuhoHarada <yuhoharada0616aaa@gmail.com>
2020-09-09 10:33:07 +0200

    ver. 2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const navList = require('./nav.json')
+const fs = require("fs")
+const bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs')
 
@@ -11,13 +14,77 @@ app.listen(3001, () => {
 app.use(express.static('public'))
 
 navList.forEach(elt => {
-    app.get(elt.url, (req, res) => {
+    let url = elt.url
+    if (elt.name == "Menu") url = "/menu/:id"
+    app.get(url, (req, res) => {
         elt.data.title = `HUNGRY PEOPLE - ${elt.name}`
         elt.data.navigation = navList
-        res.render(elt.name.toLowerCase(), elt.data)
+        elt.name == "Home" ? elt.data.smooth = true : elt.data.smooth = false
+        if (elt.name == "Menu") elt.data.id = req.params.id
+        res.status(200).render(elt.name.toLowerCase(), elt.data)
     })
 })
 
+app.post('/newBook', urlencodedParser, (req, res) => {
+    const fileName = 'bookdata.json'
+    let newData = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        people: req.body.people,
+        date: req.body.date,
+        time: req.body.time,
+    }
+    console.log(newData)
+    if (!fs.existsSync(fileName)) {
+        let data = []
+        data.push(newData)
+        data = JSON.stringify(data)
+        fs.writeFile(fileName, data, (err) => {
+            if (err) throw err
+            console.log("Data written")
+        })
+    } else {
+        let myData = fs.readFileSync(fileName, 'utf-8')
+        myData = JSON.parse(myData)
+        myData.push(newData)
+        fs.writeFile(fileName, JSON.stringify(myData), (err) => {
+            if (err) throw err
+            console.log("Data written")
+        })
+    }
+    res.redirect('/booking')
+})
+
+app.post('/newContact', urlencodedParser, (req, res) => {
+    const fileName = 'contactdata.json'
+    let newData = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        message: req.body.message
+    }
+    console.log(newData)
+    if (!fs.existsSync(fileName)) {
+        let data = []
+        data.push(newData)
+        data = JSON.stringify(data)
+        fs.writeFile(fileName, data, (err) => {
+            if (err) throw err
+            console.log("Data written")
+        })
+    } else {
+        let myData = fs.readFileSync(fileName, 'utf-8')
+        myData = JSON.parse(myData)
+        myData.push(newData)
+        fs.writeFile(fileName, JSON.stringify(myData), (err) => {
+            if (err) throw err
+            console.log("Data written")
+        })
+    }
+    res.redirect('/contact')
+})
+
 app.use((req, res) => {
-    res.render('404', { title: '404', navigation: navList })
+    res.render('404', { smooth:false, title: '404', navigation: navList })
 })
